92db41ee197ece30bb068ed2314297eea815138f
Mathias Koehnke <mathias.koehnke@web.de>
2015-04-23 18:49:06 +0200

    Initial Commit

---
+++ WKImageCacheSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKImageCacheSample
+//
+//  Created by Mathias Köhnke on 23/04/15.
+//  Copyright (c) 2015 Mathias Koehnke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
