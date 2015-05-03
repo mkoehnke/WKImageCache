96cea90987fd677fc9907d8af3e0248f7d1b6991
Mathias Koehnke <mathias.koehnke@web.de>
2015-05-03 13:19:23 +0200

    Added License Headers

--- WKImageCache/WKImageCache.swift
+++ WKImageCache/WKImageCache.swift
@@ -1,10 +1,25 @@
 //
-//  WKCachedInterfaceImage.swift
-//  WKImageCacheSample
+// WKImageCache.swift
 //
-//  Created by Mathias Köhnke on 23/04/15.
-//  Copyright (c) 2015 Mathias Koehnke. All rights reserved.
+// Copyright (c) 2015 Mathias Koehnke (http://www.mathiaskoehnke.com)
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import WatchKit
 import Foundation
@@ -104,7 +119,7 @@ private class ImageCache {
                 if (success == false) {
                     self.removeImageWithCacheStrategy(self.cacheType)
                     
-                    // There seems to be a race condition in the WatchKit code when bulk adding/removing images
+                    // There seems to be a race condition in the WatchKit cache during bulk adding/removing images
                     // especially with the same name. Therefore this delay as a workaround.
                     usleep(400000)
                 }
@@ -141,7 +156,7 @@ private class ImageCache {
     }
     
     private static var cacheType : CacheType = DefaultCacheType
-    private static let cacheKey = "de.mathiaskoehnke.wkimagecache"
+    private static let cacheKey = "com.mathiaskoehnke.wkimagecache"
     private static let lockQueue = dispatch_queue_create(cacheKey, nil)
     private static var timeStamps : NSMutableDictionary?
     
